5b872873a75895c0d81716223a45b4efc8557456
mitansh99999 <mitanshdhiman86@gmail.com>
2024-09-03 11:36:43 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -1,37 +1,25 @@
-from info import BIN_CHANNEL, URL, SHOW_ADS
+from info import BIN_CHANNEL, URL
 from utils import temp
 from web.utils.custom_dl import TGCustomYield
-from database.users_chats_db import db
+from utils import get_size
 import urllib.parse
+import secrets
+import mimetypes
 import aiofiles
+import logging
 import aiohttp
 
-async def media_watch(message_id, user_id=None):
-    # Fetch the media message details
+
+async def media_watch(message_id):
     media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
     file_properties = await TGCustomYield().generate_file_properties(media_msg)
     file_name, mime_type = file_properties.file_name, file_properties.mime_type
     src = urllib.parse.urljoin(URL, f'download/{message_id}')
     tag = mime_type.split('/')[0].strip()
-
-    # Check if the user has premium access
-    SHOW_ADS = True
-    if user_id is not None:
-        show_ads = not await db.has_premium_access(int(user_id))
-
-    # Initialize the HTML content
-    html = '<h1>This is not streamable file</h1>'
-
     if tag == 'video':
         async with aiofiles.open('web/template/watch.html') as r:
             heading = 'Watch - {}'.format(file_name)
-            html_template = await r.read()
-
-            # Prepare final HTML
-            html = html_template.replace('tag', tag) % (heading, file_name, src)
-            
-            # If the user is premium, remove the ad section
-            if not SHOW_ADS:
-                html = html.replace('<!-- Ads Section Start -->', '').replace('<!-- Ads Section End -->', '')
-
+            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+    else:
+        html = '<h1>This is not streamable file</h1>'
     return html
