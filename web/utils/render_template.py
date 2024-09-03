38b5ecb16241281b354ddaa4a83683f7cb56b32c
mitansh99999 <mitanshdhiman86@gmail.com>
2024-09-03 11:42:26 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -1,25 +1,28 @@
-from info import BIN_CHANNEL, URL
+from info import BIN_CHANNEL, URL, SHOW_ADS
 from utils import temp
 from web.utils.custom_dl import TGCustomYield
-from utils import get_size
+from database.users_chats_db import db
 import urllib.parse
-import secrets
-import mimetypes
 import aiofiles
-import logging
 import aiohttp
 
-
-async def media_watch(message_id):
+async def media_watch(message_id, user_id=None):
+    # Fetch the media message details
     media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
     file_properties = await TGCustomYield().generate_file_properties(media_msg)
     file_name, mime_type = file_properties.file_name, file_properties.mime_type
     src = urllib.parse.urljoin(URL, f'download/{message_id}')
     tag = mime_type.split('/')[0].strip()
+
+    # Initialize the HTML content
+    html = '<h1>This is not streamable file</h1>'
+
     if tag == 'video':
         async with aiofiles.open('web/template/watch.html') as r:
             heading = 'Watch - {}'.format(file_name)
-            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
-    else:
-        html = '<h1>This is not streamable file</h1>'
+            html_template = await r.read()
+
+            # Prepare final HTML
+            html = html_template.replace('tag', tag) % (heading, file_name, src)
+    
     return html
