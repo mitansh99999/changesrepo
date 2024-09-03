0678f4d36a2fcdaeaa01aab8fe48ce08d1882162
mitansh99999 <mitanshdhiman86@gmail.com>
2024-09-03 09:34:00 +0530

    Update render_template.py

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -6,6 +6,8 @@ import urllib.parse
 import aiofiles
 import aiohttp
 
+AD_SCRIPT = "<script type='text/javascript' src='//pl24249468.cpmrevenuegate.com/e8/17/6d/e8176d0f0a248f9364dfe3b5f43b5c47.js'></script>"
+
 async def media_watch(message_id, user_id=None):
     # Fetch the media message details
     media_msg = await temp.BOT.get_messages(BIN_CHANNEL, message_id)
@@ -18,16 +20,18 @@ async def media_watch(message_id, user_id=None):
     html = '<h1>This is not streamable file</h1>'
 
     if tag == 'video':
-        # Read the HTML template for video streaming
         async with aiofiles.open('web/template/watch.html') as r:
             heading = 'Watch - {}'.format(file_name)
             html_template = await r.read()
 
             # Determine if ads should be shown
-            show_ads = SHOW_ADS and (user_id is None or not await db.has_premium_access(user_id))
-
-            # Define ad content
-            ad_content = '<div class="ad">Your ad content here</div>' if show_ads else ''
+            if SHOW_ADS:
+                if user_id is None or not await db.has_premium_access(user_id):
+                    ad_content = AD_SCRIPT
+                else:
+                    ad_content = ''
+            else:
+                ad_content = ''
 
             # Prepare final HTML
             html = html_template.replace('tag', tag) % (heading, file_name, src)
