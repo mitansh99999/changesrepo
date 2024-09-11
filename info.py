68a4288be11241ebf51c7beef6953dfda15ddccd
mitansh99999 <mitanshdhiman86@gmail.com>
2024-09-11 09:53:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://filestore-8n4f.onrender.com/") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "https://changesrepo.onrender.com/") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
