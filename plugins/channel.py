2c5bd40f5c9fc20cbceecbd94f5049c5b03bd816
mitansh99999 <mitanshdhiman86@gmail.com>
2025-03-30 11:55:13 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,14 +1,16 @@
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from info import INDEX_CHANNELS, INDEX_EXTENSIONS
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video
 
-
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    media = getattr(message, message.media.value, None)
-    if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
-        media.caption = message.caption
-        await save_file(media)
+    media_file = message.document or message.video
+    if not media_file:
+        return
+
+    file_name = getattr(media_file, 'file_name', '')
+    if file_name.lower().endswith(tuple(INDEX_EXTENSIONS)):
+        await save_file(message)
